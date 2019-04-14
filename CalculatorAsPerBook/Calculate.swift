2b0809baf13795416fe026df62aeb03238590e70
Danton Corbel <dantoncorbel@gmail.com>
2019-04-15 08:26:09 +1200

    Initial Commit

---
+++ CalculatorAsPerBook/Calculate.swift
@@ -0,0 +1,67 @@
+//
+//  Calculate.swift
+//  CalculatorAsPerBook
+//
+//  Created by Deb Santa Clara on 14/04/19.
+//  Copyright © 2019 Deb Santa Clara. All rights reserved.
+//
+
+import Foundation
+
+struct Calculate {
+    var display: String
+    var storeNumberOne: Double
+    var storeNumberTwo: Double
+    var storeNumberThree: Double
+    var signCase: Sign
+    var answer: Double
+    
+    mutating func displayNumber(num: String) {
+        if display == "0" {
+            display = ""
+        }
+        display += num
+        storeNumberThree = Double(display)!
+    }
+    
+    mutating func storeToNumberOne() {
+        storeNumberOne = Double(display)!
+        display = ""
+    }
+    
+    mutating func storeToNumberTwo() {
+        storeNumberTwo = Double(display)!
+        display = ""
+    }
+    
+    mutating func enumCase(sign: String) {
+        if sign == "+" {
+            signCase = .add
+        } else if sign == "-" {
+            signCase = .subtract
+        } else if sign == "x" {
+            signCase = .multiply
+        } else if sign == "÷" {
+            signCase = .divide
+        }
+    }
+    
+    mutating func makeCalculation(x: Double, y: Double, sign: Sign) {
+            switch sign {
+            case .add:
+                answer = x+y
+            case .subtract:
+                answer = x-y
+            case .divide:
+                answer = x/y
+            case .multiply:
+                answer = x*y
+        }
+    }
+}
+
+enum Sign {
+    case add, subtract, divide, multiply
+    
+    
+}
---
+++ CalculatorAsPerBook/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  CalculatorAsPerBook
+//
+//  Created by Deb Santa Clara on 4/04/19.
+//  Copyright © 2019 Deb Santa Clara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var calculate: Calculate!
+
+    
+    //Outlets
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var allClearButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet var signButtons: [UIButton]!
+    @IBOutlet var allButtons: [UIButton]!
+    
+    //Actions
+    @IBAction func numberButtons(_ sender: UIButton) {
+        let num = sender.title(for: .normal)!
+        calculate.displayNumber(num: num)
+        displayLabel.text = calculate.display
+        allClearButton.isHidden = true
+        clearButton.isHidden = false
+        resetSignColours()
+        
+        print("Number button: storeOne: \(calculate.storeNumberOne), storeTwo: \(calculate.storeNumberTwo), storeThree: \(calculate.storeNumberThree), display: \(calculate.display), number pressed: \(num)")
+    }
+    @IBAction func allClearButtonTapped(_ sender: UIButton) {
+        reset()
+    }
+    @IBAction func clearButtonTapped(_ sender: Any) {
+        clearButton.isHidden = true
+        allClearButton.isHidden = false
+        calculate.display = ""
+        displayLabel.text = "0"
+        
+        print("Clear button: storeOne: \(calculate.storeNumberOne), storeTwo: \(calculate.storeNumberTwo), storeThree: \(calculate.storeNumberThree), display: \(calculate.display)")
+    }
+    
+    @IBAction func signButtonsTapped(_ sender: UIButton) {
+        let sign = sender.title(for: .normal)!
+        resetSignColours()
+        sender.backgroundColor = UIColor.orange
+        
+        if calculate.display.isEmpty {
+            //this allows change in signs if press wrong sign button
+        }
+        else if calculate.storeNumberOne == 0 {
+            calculate.storeToNumberOne()
+            
+            print("Sign button: storeOne: \(calculate.storeNumberOne), storeTwo: \(calculate.storeNumberTwo), storeThree: \(calculate.storeNumberThree), display: \(calculate.display), sign: \(sign), signCase: \(calculate.signCase)")
+        } else if calculate.storeNumberTwo == 0 {
+            calculate.storeToNumberTwo()
+            calculate.makeCalculation(x: calculate.storeNumberOne, y: calculate.storeNumberTwo, sign: calculate.signCase)
+            displayLabel.text = String(calculate.answer)
+            calculate.storeNumberOne = calculate.answer
+            calculate.display = "0"
+            
+            print("Sign button: storeOne: \(calculate.storeNumberOne), storeTwo: \(calculate.storeNumberTwo), storeThree: \(calculate.storeNumberThree), display: \(calculate.display), sign: \(sign), answer: \(calculate.answer)")
+            
+            calculate.storeNumberTwo = 0
+        }
+        calculate.enumCase(sign: sign)
+       
+    }
+    
+    @IBAction func equalsButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func alternativeSignButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func percentSignTapped(_ sender: UIButton) {
+    }
+    
+    
+    //FUNCTIONS
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        for button in allButtons {
+            button.layer.cornerRadius = 5
+        }
+        reset()
+    }
+    
+    func reset() {
+        clearButton.isHidden = true
+        allClearButton.isHidden = false
+        calculate = Calculate(display: "", storeNumberOne: 0, storeNumberTwo: 0, storeNumberThree: 0, signCase: .add, answer: 0)
+        displayLabel.text = "0"
+        
+        print("Reset: storeOne: \(calculate.storeNumberOne), storeTwo: \(calculate.storeNumberTwo), storeThree: \(calculate.storeNumberThree), display: \(calculate.display)")
+    }
+
+    func resetSignColours() {
+        for button in signButtons {
+            button.backgroundColor = UIColor.red
+        }
+    }
+}
+
